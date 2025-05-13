4680a13348304bc6509fe437014f2f880504dd69
MSellaX <miguelsellaprado10@gmail.com>
2025-05-13 14:46:01 -0300

    commit

--- nome.js
+++ nome.js
@@ -1,15 +1,15 @@
 let primeiroNome = "";
 let sobrenome = "";
 
-// Pede o primeiro nome até ser preenchido
-while (primeiroNome === "") {
-  primeiroNome = prompt("Digite seu nome:");
-}
+// Pede o primeiro nome até ser válido (não vazio e não número)
+do {
+  primeiroNome = prompt("Digite seu nome (você não pode se chamar números):");
+} while (primeiroNome === "" || !isNaN(primeiroNome));
 
-// Pede o sobrenome até ser preenchido
-while (sobrenome === "") {
-  sobrenome = prompt("Agora digite seu sobrenome:");
-}
+// Pede o sobrenome até ser válido (não vazio e não número)
+do {
+  sobrenome = prompt("Agora digite seu sobrenome (você não pode se chamar números):");
+} while (sobrenome === "" || !isNaN(sobrenome));
 
 // Mostra o nome completo
 alert("Seu Nome completo: " + primeiroNome + " " + sobrenome);
